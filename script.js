c2f2c820deff9a3ade4806ffacc7c4eb048787e8
abdelmoaize <abdelmouaize@gmail.com>
2022-03-26 21:38:07 +0100

    finally working, can get data from api and display it as a graph with chart.js

--- script.js
+++ script.js
@@ -1,20 +1,87 @@
 let aside = document.querySelector("aside")
 let main = document.querySelector("main")
+const ctx = document.getElementById('myChart').getContext('2d');
 let req = new XMLHttpRequest()
 req.open("GET","https://api.covid19api.com/countries")
 req.send()
 req.onreadystatechange=proccess
 let resp;
-
-function showdata(element) {
+let data = '';
+let xValues = [];
+let actives = [];
+let deaths=[]
+let confirmed = []
+let recovered=[]
+function showData() {
+    let obj ={type: 'line',
+        data: 
+        {
+            labels: xValues,
+            datasets: [
+                {
+                label: 'deaths',
+                data: deaths,
+                borderColor:['black'],
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 0
+                },
+                {
+                label: 'active',
+                data: actives,
+                borderColor: ['darkblue'],
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 0
+                },
+                {
+                label: 'recovered',
+                data: recovered,
+                borderColor: ['green'],
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 0
+                },
+                {
+                label: 'confirmed',
+                data: confirmed,
+                borderColor: ['red'],
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 0
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
+    let myChart = new Chart(ctx, obj);
+}
+function getData(element) {
     let url = `https://api.covid19api.com/dayone/country/${element.target.getAttribute("id")}`
-    console.log(url)
     let r=new XMLHttpRequest()
     r.open("GET",url)
     r.send()
     function p() {
+        console.log(url)
         if(r.readyState==4 && r.status==200){
-            let data = JSON.parse(r.response);
+            xValues=[]
+            actives=[]
+            data = JSON.parse(r.response);
+            console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                xValues.push(i.toString())
+                actives.push(data[i].Active)
+                deaths.push(data[i].Deaths)
+                confirmed.push(data[i].Confirmed)
+                recovered.push(data[i].Recovered)
+            }
+            showData()
         }
     }
     r.onreadystatechange=p;
@@ -29,7 +96,7 @@ function proccess(){
         aside.innerHTML=str
     let countries = document.querySelectorAll(".country")
     countries.forEach(element => {
-        element.addEventListener("click",showdata);
+        element.addEventListener("click",getData);
     });
 
     }
